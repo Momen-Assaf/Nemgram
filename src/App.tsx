46136b0ccd0963a79981a24115f30f566ac75939
Momen-Assaf <momenssf@gmail.com>
2023-12-18 19:15:32 +0200

    topbar +logout functions

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import { SigninForm } from './_auth/forms/SigninForm';
 import { SignupForm } from './_auth/forms/SignupForm';
 import { Home } from './_root/pages';
-import './globals.css';
+import "./globals.css";
 import { AuthLayout } from './_auth/AuthLayout';
 import RootLayout from './_root/RootLayout';
 import { Toaster } from "@/components/ui/toaster"
